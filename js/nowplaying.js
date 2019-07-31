4d954beacda73149f6e60a91416485b994c7a9b7
Afonso Santos <afonso.faisca1@gmail.com>
2019-07-31 18:32:37 +0100

    Pequena alteração no script nowplaying.js

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
    */
 
   const sub = new NchanSubscriber(
-    'https://painel.radio-afonsosantos.tk/api/live/nowplaying/radioafonsosantos'
+    "https://painel.radio-afonsosantos.tk/api/live/nowplaying/radioafonsosantos"
   );
   var resposta;
 
@@ -33,27 +33,48 @@ $(document).ready(function() {
    */
 
   // A Tocar
-  const artista = document.getElementById('artista');
-  const musica = document.getElementById('musica');
-  const imagem = document.getElementById('imagem');
-  const status = document.getElementById('status');
-  const dj = document.getElementById('dj');
-  const ouvintes = document.getElementById('ouvintes');
+  const artista = document.getElementById("artista");
+  const musica = document.getElementById("musica");
+  const imagem = document.getElementById("imagem");
+  const status = document.getElementById("status");
+  const dj = document.getElementById("dj");
+  const ouvintes = document.getElementById("ouvintes");
 
   // Vai Tocar
-  const vai_tocar_1 = document.getElementById('vai-tocar-1');
-  const vai_tocar_2 = document.getElementById('vai-tocar-2');
-  const imagem_seguinte = document.getElementById('imagem-seguinte');
-  const musica_seguinte = document.getElementById('musica-seguinte');
-  const artista_seguinte = document.getElementById('artista-seguinte');
+  const vai_tocar_1 = document.getElementById("vai-tocar-1");
+  const vai_tocar_2 = document.getElementById("vai-tocar-2");
+  const imagem_seguinte = document.getElementById("imagem-seguinte");
+  const musica_seguinte = document.getElementById("musica-seguinte");
+  const artista_seguinte = document.getElementById("artista-seguinte");
 
   // Obtém os dados apartir da Websocket
-  sub.on('message', function(message) {
+  sub.on("message", function(message) {
+    // Resposta da API
     resposta = JSON.parse(message);
+
+    /*
+     *  Atualiza sempre estes dados abaixo
+     */
+
+    // A Tocar
+    $(imagem).attr("src", resposta.now_playing.song.art);
+    $(artista).text(resposta.now_playing.song.artist);
+    $(musica).text(resposta.now_playing.song.title);
+
+    // Vai Tocar
+    $(imagem_seguinte).attr("src", resposta.playing_next.song.art);
+    $(artista_seguinte).text(resposta.playing_next.song.artist);
+    $(musica_seguinte).text(resposta.playing_next.song.title);
+
+    /*
+     *  Funcionalidades condicionadas pela resposta
+     *  da API
+     */
+
     // Verifica se algum DJ está a transmitir
     if (resposta.live.is_live == true) {
       // DJ Online
-      $(status).text('DJ ao Vivo');
+      $(status).text("DJ ao Vivo");
       // Mostra o nome do DJ
       $(dj).show();
       $(dj).text(resposta.live.streamer_name);
@@ -64,7 +85,7 @@ $(document).ready(function() {
       // Transmissão Normal
       // Esconde o nome do DJ, depois de terminar a transmissão
       $(dj).hide();
-      $(status).text('A Tocar');
+      $(status).text("A Tocar");
       // Mostra os elementos do Vai Tocar
       $(vai_tocar_1).show();
       $(vai_tocar_2).show();
@@ -72,11 +93,11 @@ $(document).ready(function() {
 
     // Verifica se é "ouvinte" ou "ouvintes"
     if (resposta.listeners.current > 1) {
-      $(ouvintes).text(resposta.listeners.unique + ' ouvintes');
+      $(ouvintes).text(resposta.listeners.unique + " ouvintes");
     } else if (resposta.listeners.current == 1) {
-      $(ouvintes).text(resposta.listeners.unique + ' ouvinte');
+      $(ouvintes).text(resposta.listeners.unique + " ouvinte");
     } else if (resposta.listeners.unique == 0) {
-      $(ouvintes).text('Nenhum Ouvinte');
+      $(ouvintes).text("Nenhum Ouvinte");
     }
 
     /*
@@ -85,32 +106,18 @@ $(document).ready(function() {
      */
 
     // Playlists
-    const playlist_variada = document.getElementById('playlist-variada');
-    const playlist_synthwave = document.getElementById('playlist-synthwave');
+    const playlist_variada = document.getElementById("playlist-variada");
+    const playlist_synthwave = document.getElementById("playlist-synthwave");
     var playlist_atual = resposta.now_playing.playlist;
 
     // Verifica a o nome da playlist e seleciona o elemento correto
-    if (playlist_atual == 'Músicas - Variadas') {
-      $(playlist_synthwave).removeClass('active');
-      $(playlist_variada).addClass('active');
-    } else if (playlist_atual == 'Músicas - Synthwave') {
-      $(playlist_variada).removeClass('active');
-      $(playlist_synthwave).addClass('active');
+    if (playlist_atual == "Músicas - Variadas") {
+      $(playlist_synthwave).removeClass("active");
+      $(playlist_variada).addClass("active");
+    } else if (playlist_atual == "Músicas - Synthwave") {
+      $(playlist_variada).removeClass("active");
+      $(playlist_synthwave).addClass("active");
     }
-
-    /*
-     *  Atualiza sempre estes dados abaixo
-     */
-
-    // A Tocar
-    $(imagem).attr('src', resposta.now_playing.song.art);
-    $(artista).text(resposta.now_playing.song.artist);
-    $(musica).text(resposta.now_playing.song.title);
-
-    // Vai Tocar
-    $(imagem_seguinte).attr('src', resposta.playing_next.song.art);
-    $(artista_seguinte).text(resposta.playing_next.song.artist);
-    $(musica_seguinte).text(resposta.playing_next.song.title);
   });
 
   // Inicia o nchan
