7c6caa26f4199d89dced3e5b95d4b0009537c1a1
Afonso Santos <afonso.faisca1@gmail.com>
2019-08-01 12:35:01 +0100

    Não ocultar a música seguinte quando um DJ estãoa vivo

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -41,8 +41,6 @@ $(document).ready(function() {
   const ouvintes = document.getElementById("ouvintes");
 
   // Vai Tocar
-  const vai_tocar_1 = document.getElementById("vai-tocar-1");
-  const vai_tocar_2 = document.getElementById("vai-tocar-2");
   const imagem_seguinte = document.getElementById("imagem-seguinte");
   const musica_seguinte = document.getElementById("musica-seguinte");
   const artista_seguinte = document.getElementById("artista-seguinte");
@@ -78,17 +76,11 @@ $(document).ready(function() {
       // Mostra o nome do DJ
       $(dj).show();
       $(dj).text(resposta.live.streamer_name);
-      // Oculta os elementos do Vai Tocar, para prevenir informação errada
-      $(vai_tocar_1).hide();
-      $(vai_tocar_2).hide();
     } else {
       // Transmissão Normal
       // Esconde o nome do DJ, depois de terminar a transmissão
       $(dj).hide();
       $(status).text("A Tocar");
-      // Mostra os elementos do Vai Tocar
-      $(vai_tocar_1).show();
-      $(vai_tocar_2).show();
     }
 
     // Verifica se é "ouvinte" ou "ouvintes"
